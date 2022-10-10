5552a63d398f6d3f61c1460b5f998765a99419cd
Siarhei Paulavets <s.paulavets@gmail.com>
2022-10-10 21:13:09 +0300

    Add DateTime generator

---
+++ Faking.Core/Generators/DateTimeGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace Faking.Core.Generators;
+
+public class DateTimeGenerator : IValueGenerator
+{
+    public object Generate(Type typeToGenerate, GeneratorContext context) =>
+        new DateTime(context.Random.NextInt64(DateTime.MinValue.Ticks, DateTime.MaxValue.Ticks));
+
+    public Type GetGeneratedType() => typeof(DateTime);
+}
