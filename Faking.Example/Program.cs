6ae624ed540731bd2da886c9397fee8742a5d0ec
Siarhei Paulavets <s.paulavets@gmail.com>
2022-10-10 12:11:09 +0300

    Add builtins test

--- Faking.Example/Program.cs
+++ Faking.Example/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Type Bar(Type type)
+{
+    return type;
+}
+
+Type Foo<T>()
+{
+    return Bar(typeof(T));
+}
+
+Console.WriteLine(Foo<int>());
+Console.WriteLine(Bar(typeof(int)));
--- Faking.Tests/FakerTests.cs
+++ Faking.Tests/FakerTests.cs
@@ -1,11 +1,36 @@
+using Faking.Core;
+
 namespace Faking.Tests;
 
 public class FakerTests
 {
+    const int M = 30; // Min number of unique objects in each collection
+    const int N = 50; // Number of test objects in each collection
+    
     [Fact]
     public void BuiltinTest()
     {
-        throw new NotImplementedException();
+        // Arrange
+        Faker faker = new();
+        bool[] booleans = new bool[N];
+        int[] integers = new int[N];
+        double[] doubles = new double[N];
+        string[] strings = new string[N];
+
+        // Act
+        for (int i = 0; i < N; i++)
+        {
+            booleans[i] = faker.Create<bool>();
+            integers[i] = faker.Create<int>();
+            doubles[i] = faker.Create<double>();
+            strings[i] = faker.Create<string>();
+        }
+
+        // Assert
+        Assert.True(booleans.All(_ => false));
+        Assert.True(integers.Distinct().Count() >= M);
+        Assert.True(doubles.Distinct().Count() >= M);
+        Assert.True(strings.Distinct().Count() >= M);
     }
 
     [Fact]
