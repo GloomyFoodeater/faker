7fa7cca94e9afcc9f44dbdd2029a684e9f093f5b
Siarhei Paulavets <s.paulavets@gmail.com>
2022-10-11 16:53:37 +0300

    Add FakerConfig

---
+++ Faking.Core/FakerConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Faking.Core;
+
+public class FakerConfig
+{
+    public Dictionary<MemberInfo, IValueGenerator> MembersGenerators { get; } = new();
+
+    public void Add<TSource, TMember, TGenerator>(Expression<Func<TSource, TMember>> expression)
+    {
+        var memberExpression = (MemberExpression)expression.Body;
+        var generator = (IValueGenerator?)Activator.CreateInstance<TGenerator>();
+        if(generator != null)
+            MembersGenerators[memberExpression.Member] = generator;
+
+    }
+}
